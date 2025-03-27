e59e68a567b43fd2fa2ffea33ef75a8696070839
Ksenija07 <ksenijabidzane@gmail.com>
2025-03-27 21:47:13 +0200

    Setter structure

--- RPGButtonGame/Assets/Scripts/PoisonedDagger.cs
+++ RPGButtonGame/Assets/Scripts/PoisonedDagger.cs
@@ -7,21 +7,31 @@ public class PoisonedDagger : Weapon
     [SerializeField] private int poisonDamage = 2;
     private GameManager gameManager;
 
+    // Property for Poison Damage
+    private int PoisonDamage
+    {
+        get => poisonDamage;
+        set
+        {
+            poisonDamage = Mathf.Max(0, value); // Ensures poison damage is not negative
+        }
+    }
+
     private void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
     }
+
     public override void ApplyEffect(Character character)
     {
-        int totalDamage = 0;
-        totalDamage += Random.Range(minDamage, maxDamage + 1) + poisonDamage;
-        Debug.Log("Added poison damage: " + totalDamage);
+        int totalDamage = Random.Range(minDamage, maxDamage + 1) + PoisonDamage;
+        Debug.Log($"Added poison damage: {totalDamage}");
         character.GetHit(totalDamage);
     }
 
     public void SelectWeapon(Character character)
     {
-        character.EquipWeapon(this); 
+        character.EquipWeapon(this);
         Debug.Log("Poisoned Dagger selected!");
 
         if (gameManager != null)
@@ -29,5 +39,4 @@ public class PoisonedDagger : Weapon
             gameManager.UpdateSelectedWeaponText("Poisoned Dagger");
         }
     }
-
 }
