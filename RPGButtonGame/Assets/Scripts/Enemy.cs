b9942eaf5fd9e2270e35e3bcef24be94139289aa
Ksenija07 <ksenijabidzane@gmail.com>
2025-03-27 20:21:50 +0200

    Last changes

--- RPGButtonGame/Assets/Scripts/Enemy.cs
+++ RPGButtonGame/Assets/Scripts/Enemy.cs
@@ -5,33 +5,10 @@ using UnityEngine;
 public class Enemy : Character
 {
     [SerializeField] internal int aggresion = 10;
-   /* public GameObject berserker;
-    public GameObject goblin;
-    public GameObject archer;*/
 
     private void Die()
     {
         Debug.Log("Enemy has died!");
-        //RespawnEnemy();
+       
     }
-
-    /*private void RespawnEnemy()
-    {
-        int randomEnemy = Random.Range(0, 3);
-
-        GameObject enemyToRespawn = null;
-
-        switch (randomEnemy)
-        {
-            case 0:
-                enemyToRespawn = berserker;
-                break;
-            case 1:
-                enemyToRespawn = goblin;
-                break;
-            case 2:
-                enemyToRespawn = archer;
-                break;
-        }
-    }*/
 }
